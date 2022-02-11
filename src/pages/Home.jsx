4ca905226fee53d3ac99ad76246b7b793b542d6e
Léia Ribeiro <leiaribeirot@gmail.com>
2022-02-11 09:59:59 -0400

    Requisito 3 - Correçao de Lint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 import Input from '../components/Input';
 import CategoriesSidebar from '../components/CategoriesSidebar';
 import CardList from '../components/CardList';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 class Home extends Component {
   render() {
