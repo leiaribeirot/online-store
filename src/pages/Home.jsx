6815e3f3ddb759f3ba87cd816739f32f142737b3
Gustavo-Ferrari <gustavod.ferrari@gmail.com>
2022-02-11 17:03:36 -0300

    estiliza fundo e borda

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import CardList from '../components/CardList';
 import Header from '../components/Header';
 import '../Styles/Header.css';
 import '../Styles/allProducts.css';
+import '../Styles/Home.css';
 
 class Home extends Component {
   render() {
@@ -20,30 +21,36 @@ class Home extends Component {
     } = this.props;
 
     return (
-      <div>
+      <main>
         <Header />
-        <Input
-          type="text"
-          labelId="home-initial-message"
-          inputId="query-input"
-          labelText="Digite algum termo de pesquisa ou escolha uma categoria."
-          value={ searchInput }
-          name="searchInput"
-          onChange={ handleChange }
-          onKeyDown={ handleOnKeyDown }
-        />
-        <button
-          type="button"
-          data-testid="query-button"
-          onClick={ handleClick }
-        >
-          Pesquisar
-        </button>
-        <div className="allProducts">
-          <section className="sideBar"><CategoriesSidebar { ...this.props } /></section>
-          <section className="productsList"><CardList { ...this.props } onAddProduct={ handleAddProduct } /></section>
-        </div>
-      </div>
+        <body className="body">
+          <Input
+            type="text"
+            labelId="home-initial-message"
+            inputId="query-input"
+            labelText="Digite algum termo de pesquisa ou escolha uma categoria."
+            value={ searchInput }
+            name="searchInput"
+            onChange={ handleChange }
+            onKeyDown={ handleOnKeyDown }
+          />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ handleClick }
+          >
+            Pesquisar
+          </button>
+          <div className="allProducts">
+            <section className="sideBar">
+              <CategoriesSidebar { ...this.props } />
+            </section>
+            <section className="productsList">
+              <CardList { ...this.props } onAddProduct={ handleAddProduct } />
+            </section>
+          </div>
+        </body>
+      </main>
     );
   }
 }
