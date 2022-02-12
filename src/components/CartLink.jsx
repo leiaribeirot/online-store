a7ed2df0803236151feeecc985c4903c4ea579fd
Léia Ribeiro <leiaribeirot@gmail.com>
2022-02-12 00:31:00 -0400

    Req 3 - Criaçao do Botao Carrinho e Home - Comentado e Ajustado

--- src/components/CartLink.jsx
+++ src/components/CartLink.jsx
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export default class CartLink extends Component {
   render() {
-    const { itemCount } = this.props;
-
     return (
       <Link
-        key={ itemCount }
-        className="cart-link"
         to="/cart"
         data-testid="shopping-cart-button"
       >
         <AiOutlineShoppingCart />
-        <span data-testid="shopping-cart-size">{itemCount}</span>
       </Link>
     );
   }
 }
 
-CartLink.propTypes = {
-  itemCount: PropTypes.number.isRequired,
-};
+// No link precisa redicionar para a página que você quer Exibir a busca que no caso
+// é a página CART com a mensagem exigina no Requisito
